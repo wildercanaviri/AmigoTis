3fb8f2584699943390498a2ce78bf5e9fe90c7a0
wildercanaviri <wildersistemas20@gmail.com>
2019-12-15 23:12:20 -0400

    mejora de vistas

--- app/Role.php
+++ app/Role.php
@@ -5,9 +5,9 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
-{   /*
+{   
     protected $fillable = ["nom_rol","descripcion"];
-    
+    /* 
     public function usuarios(){
         //return $this->belongsToMany('App\User','role_user','role_id','user_id');
         return $this->belongsToMany('App\User');
--- resources/views/usuarios/show.blade.php
+++ resources/views/usuarios/show.blade.php
@@ -32,14 +32,45 @@
                 </nav>
 @endsection
 @section("contenido")
-<div style="color: white;">
-	<h1>{{$usuario->nom_usu}}</h1>
-    <h1>{{$usuario->ape_usu}}</h1>
-    <h2>{{$usuario->email}}</h1>
-    <h2>{{$usuario->fecha_nac}}</h1>
-    <h2>{{$usuario->tel_usu}}</h1>
-    <h1>{{$role}}</h1>
-</div>
+ <div class="container">
+  <div class="row">
+    <from>
+    <div class="form-group" class="col-6" >
+      <label for="nombre_usu" style="color: white;">Nombre de Usuario</label>
+      <input type="text" class="form-control" id="nombre_usu" readonly placeholder = "{{$usuario->nom_usu}}">
+    </div>
+    <div class="form-group" class="col-6">
+      <label for="apellido_usu" style="color: white;">Apellido de Usuario</label>
+      <input type="text" class="form-control" id="apellido_usu" readonly placeholder="{{$usuario->ape_usu}}">
+    </div>
+    <div class="form-group" class="col-6">
+      <label for="email" style="color: white;">Email </label>
+      <input type="text" class="form-control" id="email" readonly placeholder="{{$usuario->email}}">
+    </div>
+    <div class="form-group" class="col-6">
+      <label for="fecha_naci" style="color: white;">Fecha de Nacimiento</label>
+      <input type="text" class="form-control" id="fecha_naci" readonly placeholder="{{$usuario->fecha_nac}}">
+    </div>
+    <div class="form-group"class="col-6">
+      <label for="telefono" style="color: white;">Telefono </label>
+      <input type="text" class="form-control" id="fecha_naci" readonly placeholder="{{$usuario->tel_usu}}">
+    </div>
+    <div class="form-group" >
+       <label for="rol" style="color: white;">Roles</label>
+      @foreach($usuario->roles as $rol)
+        <input type="text" class="form-control" id="rol" readonly placeholder="{{$usuario->roles}}">
+      @endforeach    
+    </div>
+   </from>
+   </div> 
+  </div>
     
     
+    
+    
+
+
+  
+ 
+   
 @endsection
